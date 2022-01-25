fcd609f60ee0b6811355bc20457e7d2d74f0dd02
luchux <lucianoguasco@gmail.com>
2022-01-25 16:09:37 -0300

    moving stuff

--- src/hooks/Guilds/ether-swr/useProposal.ts
+++ src/hooks/Guilds/ether-swr/useProposal.ts
@@ -1,29 +1,10 @@
 import useEtherSWR from 'ether-swr';
-import { unix } from 'moment';
-import { Middleware, SWRHook } from 'swr';
+import { proposalFormatterMiddleware } from './middleware/proposalFormatter';
+
 import { Proposal } from '../../../types/types.guilds';
-
-const formatterMiddleware: Middleware =
-  (useSWRNext: SWRHook) => (key, fetcher, config) => {
-    const swr = useSWRNext(key, fetcher, config);
-    if (swr.data) {
-      const original = swr.data as any;
-
-      const clone: any = Object.assign({}, swr.data);
-      clone.startTime = original.startTime
-        ? unix(original.startTime.toNumber())
-        : null;
-      clone.endTime = original.endTime
-        ? unix(original.endTime.toNumber())
-        : null;
-
-      return { ...swr, data: clone };
-    }
-    return swr;
-  };
 
 export const useProposal = (guildId: string, proposalId: string) => {
   return useEtherSWR<Proposal>([guildId, 'getProposal', proposalId], {
-    use: [formatterMiddleware],
+    use: [proposalFormatterMiddleware],
   });
 };
